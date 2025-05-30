e11925e785e50aa2496bb6f07d2d5986ee1c2bd8
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2025-05-30 17:34:23 -0400

    a

--- src/main/java/com/springbootexpert/springbootexpert/controller/ProductController.java
+++ src/main/java/com/springbootexpert/springbootexpert/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.springbootexpert.springbootexpert.controller;
 
 import com.springbootexpert.springbootexpert.model.Product;
 import com.springbootexpert.springbootexpert.model.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,19 +25,41 @@ public class ProductController {
         return product;
     }
 
-    @GetMapping({"", "/"})
-    public String ok() {
-        return "<h1>Deu certo cara</h1>";
+    @GetMapping({"", "/", "/all"})
+    public ResponseEntity<?> getAll(){
+        List<Product> products = productRepository.findAll();
+        if (products.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Nenhum produto encontrado.");
+        } else {
+            return ResponseEntity.ok(products);
+        }
     }
 
-    @GetMapping("/all")
-    public List<Product> getAll(){
-        return productRepository.findAll();
-    }
+
 
     @GetMapping("/id/{product_id}")
     public Product getById(@PathVariable String product_id) {
         Optional<Product> product = productRepository.findById(product_id);
         return product.orElse(null);
+    }
+
+    @DeleteMapping("{product_id}")
+    public ResponseEntity<String> deleteById(@PathVariable String product_id){
+        if (productRepository.existsById(product_id)) {
+            productRepository.deleteById(product_id);
+            return ResponseEntity.ok("Produto deletado com sucesso");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado");
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Product> update(@RequestBody Product product){
+        if (product.getProduct_id() == null || !productRepository.existsById(product.getProduct_id())) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        Product updatedProduct = productRepository.save(product);
+        return ResponseEntity.ok(updatedProduct);
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,5 @@ create table Product (
     price decimal(10, 2) not null,
     primary key (product_id)
 );
+
+INSERT INTO Product (product_id, name, description, price) VALUES ('123', 'Consolo GG', 'Penis bem grande e grosso', 79.99)
